18799252a8afd9aa4d5a2e898c25b8d61b26d1f6
Luca Stanger <lstanger@gmx.de>
2020-03-23 14:07:50 +0100

    Subclassing UITableViewController

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Luca Stanger on 23.03.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
