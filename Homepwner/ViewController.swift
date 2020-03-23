e6f0362f5a344b154755e3b7a0a41b1f449681dc
Luca Stanger <lstanger@gmx.de>
2020-03-23 14:02:13 +0100

    Initial Commit

---
+++ Homepwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Luca Stanger on 23.03.20.
+//  Copyright © 2020 Luca Stanger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
