58ce8bb1862ad0b005eabd72b588288a70a5a36c
Luca Stanger <lstanger@gmx.de>
2020-03-30 15:20:48 +0200

    Appearing and Disappearing Views

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -116,4 +116,10 @@ class ItemsViewController: UITableViewController {
             preconditionFailure("Unexpected segue identifier")
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
 }
